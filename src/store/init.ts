1f34019313b11121e350d023c6cc58405cdb8171
Timo Reitz <godsboss@gmx.de>
2018-11-14 02:05:41 +0100

    Get rid of useless Action interface.

--- src/store/init.ts
+++ src/store/init.ts
@@ -1,13 +1,11 @@
-export function create(game: Game): (state: State, action: Action) => State {
-  return function(state: State, action: Action) {
+export function create(game: Game): (state: State, action: any) => State {
+  return function(state: State, action: any) {
     if (state === undefined) {
       return initialState
     }
     return state
   }
 }
-
-interface Action {}
 
 interface Game {}
 
