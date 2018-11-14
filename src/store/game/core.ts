2fa4896798dea8f54f1a84035ec9d0532ee1550a
Timo Reitz <godsboss@gmx.de>
2018-11-14 02:41:14 +0100

    Add some important interfaces and a function for creating a fresh state.

---
+++ src/store/game/core.ts
@@ -0,0 +1,51 @@
+export function freshState(): State {
+  return {
+    turn: 1,
+    buildings: [],
+    deposits: [],
+    resources: [],
+    units: [],
+  }
+}
+
+export interface State {
+  turn: number
+  buildings: Building[]
+  deposits: Deposit[]
+  resources: Resource[]
+  units: Unit[]
+}
+
+interface Building {
+  id: string
+  name: string
+  level: number
+}
+
+interface Deposit {
+  id: string
+  name: string
+  amount: number
+  harvested: number
+}
+
+interface Resource {
+  id: string
+  name: string
+  amount: number
+  spent: number
+  visible: boolean
+}
+
+interface Unit {
+  id: string
+  name: string
+  amount: number
+  lost: number
+  reservoir: UnitReservoir
+}
+
+interface UnitReservoir {
+  current: number
+  wasted: number
+}
