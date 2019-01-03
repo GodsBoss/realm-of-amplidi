44abe7c4bb912c11bc929f597cb459eed7090025
Timo Reitz <godsboss@gmx.de>
2019-01-03 21:31:52 +0100

    Show construct/upgrade cost for buildings (no names yet, only IDs).

--- src/components/buildings.tsx
+++ src/components/buildings.tsx
@@ -1,4 +1,4 @@
-import { Building as BuildingTemplate } from '../store/game/template'
+import { Building as BuildingTemplate, Game } from '../store/game/template'
 import { BuildingMap, Building as BuildingValue } from '../store/game/state'
 import * as React from 'react' // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/5128
 
@@ -19,9 +19,29 @@ export const Buildings = ({list, values, onLevelBuilding}: {list: BuildingTempla
 
 const Building = ({building, value, onLevelBuilding}: {building: BuildingTemplate, value: BuildingValue, onLevelBuilding: OnLevelBuilding }) => (
   <li key={ building.id } className={ value.visible ? 'visible' : 'invisible' }>
-    { building.name } Level { value.level } <button onClick={ () => { onLevelBuilding(building.id) } } disabled={ !value.available }>{value.level === 0 ? 'Construct' : 'Upgrade'}</button>
+    <BuildingInfo name={building.name} level={value.level} />
+    <BuildingUpgrade building={building} value={value} onLevelBuilding={onLevelBuilding} />
   </li>
 )
+
+const BuildingInfo = ({name, level}: { name: string, level: number }) => (
+  <span className="building-info">{ name } Level { level }</span>
+)
+
+const BuildingUpgrade = ({building, value, onLevelBuilding}: {building: BuildingTemplate, value: BuildingValue, onLevelBuilding: OnLevelBuilding }) => {
+  if (building.levels.length > value.level) {
+    return <span className="building-upgrade">
+      <button onClick={ () => { onLevelBuilding(building.id) } } disabled={ !value.available }>{value.level === 0 ? 'Construct' : 'Upgrade'}</button>
+      {
+        Object.keys(building.levels[value.level].cost).map(
+          (id) => <span className="cost-item">{`${building.levels[value.level].cost[id]} ${id}`}</span>
+        )
+      }
+    </span>
+  } else {
+    return <span className="building-upgrade"></span>
+  }
+}
 
 interface OnLevelBuilding {
   (id: string): void
